0d5f5afb3fe688aaa4af8b4513a4ab21c7f4d019
daichi0918 <kayasugadaichi@icloud.com>
2025-12-28 19:37:36 +0900

    LoginTemplateの雛形作成

---
+++ fe/features/auth/components/LoginTemplate/LoginTemplate.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react';
+
+export const LoginTemplate: FC = () => {
+  return (
+    <div className="w-[90%] mx-auto min-[960px]:w-1/2">
+      <h1 className="text-center font-title text-white text-[48px]">Login</h1>
+      <form className="w-[80%] mx-auto my-10">
+        <div className="mt-10"></div>
+        <div className="mt-10"></div>
+        <div className="mt-10"></div>
+        <div className="mt-10"></div>
+      </form>
+    </div>
+  );
+};
